9594769f75364479cfa9b846bf129d71d37e4681
jmontenegro <jamzdeveloper@gmail.com>
2024-01-12 01:21:26 -0500

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   env: {
     GRAPHQL_EURL_API: process.env.GRAPHQL_EURL_API,
     API_URL: process.env.API_URL,
+    NEXT_PUBLIC_API_URL:process.env.NEXT_PUBLIC_API_URL
   },
 };
 
